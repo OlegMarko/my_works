7b4f7a6a7ea5a265a4c0ee2f944672101e655452
OlegMarko <olegmarko7@gmail.com>
2016-09-27 00:03:37 +0300

    ...

---
+++ SendMailsApp/routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::post('/sendmail', function (\Illuminate\Http\Request $request, \Illuminate\Mail\Mailer $mailer) {
+    $mailer->
+        to($request->input('email'))
+        ->send(new App\Mail\MyMail($request->input('title')));
+    return redirect()->back();
+})->name('sendmail');
